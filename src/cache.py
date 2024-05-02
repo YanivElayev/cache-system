f2671efc516ce8cf9ac9795d1ca2afcb4c787d1e
Yaniv Elayev <ivielayev@gmail.com>
2024-05-02 17:04:42 +0300

    Add abstract cache

---
+++ src/cache.py
@@ -0,0 +1,12 @@
+class Cache:
+    def __init__(self, max_size: int):
+        self.max_size = max_size
+
+    def get(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def delete(self, key):
+        pass
