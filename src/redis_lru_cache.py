03669761ebcd3d2e722bcdd6e8d07c93ee530e34
Yaniv Elayev <ivielayev@gmail.com>
2024-05-04 21:50:24 +0300

    IMplement the logic that removes large chunks before hot chunks of 8kb

--- src/redis_lru_cache.py
+++ src/redis_lru_cache.py
@@ -6,33 +6,46 @@ from typing import Callable, List
 from src.cache import Cache
 from redis import Redis
 
-from src.constants import SMALL_REQUEST_SIZE, LARGE_REQUEST_SIZE
+from src.constants import SECONDS_OF_LARGE_CHUNKS_IN_CACHE, LARGE_REQUEST_SIZE
 
 
-class RedisLRUChunksCache(Cache):
-    def __init__(self, redis_host: str, redis_port: int, max_size: int, chunk_sizes: List[int]):
+class RedisLRUChunksCache(Cache):  # TODO rename since it is not a usual lru cache
+    def __init__(self, redis_host: str, redis_port: int, chunk_sizes: List[int], max_size: int,
+                 duration_of_largest_chunk_size=SECONDS_OF_LARGE_CHUNKS_IN_CACHE):
         self.redis_client = Redis(redis_host, redis_port, decode_responses=True)
         self.chunk_sizes = chunk_sizes
         self.access_times_sorted_sets = {chunk_size: f"{chunk_size}_access_times" for chunk_size in chunk_sizes}
         self.max_size = max_size
+        self.largest_chunk_size = max(chunk_sizes)
+        self.duration_of_largest_chunks = duration_of_largest_chunk_size
 
     def get(self, offset: int, size: int):
-        self.redis_client.zadd(self.access_times_sorted_sets[size], {str(offset): time.time()})
-        # TODO don't update temporary size's access time
+        if not size == self.largest_chunk_size:
+            self.redis_client.zadd(self.access_times_sorted_sets[size], {str(offset): time.time()})
         return self.redis_client.hget(str(size), str(offset))
 
     def put(self, offset: int, value: str):
         current_size = 0
         for chunk_size in self.chunk_sizes:
-            current_size += chunk_size*self.redis_client.hlen(str(chunk_size))
-        size = len(value)
-        if current_size + size > self.max_size:
-            print("deleting older chunks")
-            offset_to_remove = self.redis_client.zrange(self.access_times_sorted_sets[size], 0, 0)[0]
-            # TODO try remove from temporary cache sizes first.
-            self.delete(offset_to_remove, size)
-        self.redis_client.zadd(self.access_times_sorted_sets[size], {str(offset): time.time()})
-        return self.redis_client.hset(str(size), str(offset), value)
+            current_size += chunk_size * self.redis_client.hlen(str(chunk_size))
+        size_of_current_chunk = len(value)
+        if current_size + size_of_current_chunk > self.max_size:
+            recent_large_chunk = \
+                self.redis_client.zrevrange(self.access_times_sorted_sets[self.largest_chunk_size], 0, 0,
+                                            withscores=True)[0]
+            seconds_since_recent_large_chunk = time.time() - recent_large_chunk[1]
+            if size_of_current_chunk != self.largest_chunk_size and seconds_since_recent_large_chunk > self.duration_of_largest_chunks:
+                size_to_remove = self.largest_chunk_size
+                offset_to_remove = recent_large_chunk[0]
+            else:
+                size_to_remove = size_of_current_chunk
+                offset_to_remove = self.redis_client.zrange(self.access_times_sorted_sets[size_of_current_chunk], 0, 0)[
+                    0]
+            print(
+                f"deleting older chunk - size: {size_to_remove}, seconds since recent large chunk: {seconds_since_recent_large_chunk}")
+            self.delete(offset_to_remove, size_to_remove)
+        self.redis_client.zadd(self.access_times_sorted_sets[size_of_current_chunk], {str(offset): time.time()})
+        return self.redis_client.hset(str(size_of_current_chunk), str(offset), value)
 
     def delete(self, offset: int, size: int):
         self.redis_client.zrem(self.access_times_sorted_sets[size], offset)
@@ -48,7 +61,7 @@ class RedisLRUChunksCache(Cache):
             else:
                 print("received from function's execution")
                 result = func(offset, size)
-                if len(result) == SMALL_REQUEST_SIZE:
+                if not len(result) == LARGE_REQUEST_SIZE:
                     self.put(offset, result)
                     large_chunk_from_requested_offset = func(offset, LARGE_REQUEST_SIZE)
                     self.put(offset, large_chunk_from_requested_offset)
