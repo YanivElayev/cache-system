90535db064915c421cc03eb601311f527b0d5b8d
Yaniv Elayev <ivielayev@gmail.com>
2024-05-04 20:28:10 +0300

    Use chunk_sizes as keys instead of previous key names

--- src/redis_lru_cache.py
+++ src/redis_lru_cache.py
@@ -1,42 +1,42 @@
 import functools
 import pickle
 import time
-from typing import Callable
+from typing import Callable, List
 
 from src.cache import Cache
 from redis import Redis
 
-from src.constants import SMALL_REQUEST_SIZE, LARGE_REQUEST_SIZE, CACHE_MAX_SIZE
+from src.constants import SMALL_REQUEST_SIZE, LARGE_REQUEST_SIZE
 
 
 class RedisLRUChunksCache(Cache):
-    def __init__(self, redis_host: str, redis_port: int, max_size: int):
+    def __init__(self, redis_host: str, redis_port: int, max_size: int, chunk_sizes: List[int]):
         self.redis_client = Redis(redis_host, redis_port, decode_responses=True)
-        self.cache_sets = {SMALL_REQUEST_SIZE: "lru_small_chunks_cache",
-                           LARGE_REQUEST_SIZE: "lru_large_chunks_cache"}
-        self.access_times_sorted_sets = {SMALL_REQUEST_SIZE: "small_chunks_access_scores",
-                                         LARGE_REQUEST_SIZE: "large_chunks_access_scores"}
+        self.chunk_sizes = chunk_sizes
+        self.access_times_sorted_sets = {chunk_size: f"{chunk_size}_access_times" for chunk_size in chunk_sizes}
         self.max_size = max_size
 
-    def get(self, offset, size):
-        self.redis_client.zadd(self.access_times_sorted_sets[size], {offset: time.time()})
-        return self.redis_client.hget(self.cache_sets[size], offset)
+    def get(self, offset: int, size: int):
+        self.redis_client.zadd(self.access_times_sorted_sets[size], {str(offset): time.time()})
+        # TODO don't update temporary size's access time
+        return self.redis_client.hget(str(size), str(offset))
 
-    def put(self, offset, value):
-        current_size = SMALL_REQUEST_SIZE * self.redis_client.hlen(
-            self.cache_sets[SMALL_REQUEST_SIZE]) + LARGE_REQUEST_SIZE * self.redis_client.hlen(
-            self.cache_sets[LARGE_REQUEST_SIZE])
+    def put(self, offset: int, value: str):
+        current_size = 0
+        for chunk_size in self.chunk_sizes:
+            current_size += chunk_size*self.redis_client.hlen(str(chunk_size))
         size = len(value)
         if current_size + size > self.max_size:
             print("deleting older chunks")
             offset_to_remove = self.redis_client.zrange(self.access_times_sorted_sets[size], 0, 0)[0]
+            # TODO try remove from temporary cache sizes first.
             self.delete(offset_to_remove, size)
-        self.redis_client.zadd(self.access_times_sorted_sets[size], {offset: time.time()})
-        return self.redis_client.hset(self.cache_sets[size], offset, value)
+        self.redis_client.zadd(self.access_times_sorted_sets[size], {str(offset): time.time()})
+        return self.redis_client.hset(str(size), str(offset), value)
 
-    def delete(self, offset, size):
+    def delete(self, offset: int, size: int):
         self.redis_client.zrem(self.access_times_sorted_sets[size], offset)
-        return self.redis_client.hdel(self.cache_sets[size], offset)
+        return self.redis_client.hdel(str(size), str(offset))
 
     def __call__(self, func: Callable):
         @functools.wraps(func)
