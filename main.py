852ddb720bfc3a47dd75451f3f1ea4962b4b9aa5
Yaniv Elayev <ivielayev@gmail.com>
2024-05-13 01:11:59 +0300

    Add examples of the cache mechanism

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.constants import SMALL_REQUEST_SIZE, LARGE_REQUEST_SIZE, FILE_PATH
+from src.db import DB
+
+
+def main():
+    db = DB(FILE_PATH)
+    db.get(100, SMALL_REQUEST_SIZE)  # reads the chunk from the file and store it in the cache.
+    # From the same offset, a 64 KB chunk will be stored for 2 seconds.
+    db.get(100, SMALL_REQUEST_SIZE)  # reads the chunk from the cache.
+    db.get(100, LARGE_REQUEST_SIZE)  # reads the chunk from the cache.
+    db.get(90, SMALL_REQUEST_SIZE)  # In case the cache is full, the oldest 64 KB chunk will be removed if there has
+    # been 2 seconds since its insertion time. Else, the least recently used 8 KB chunk will be removed from the cache
+    # in order to make room for the new chunk.
+
+
+if __name__ == '__main__':
+    main()
