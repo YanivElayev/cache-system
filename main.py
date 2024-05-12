5d17ba32d834e43d48cace9693aaa07d3b5e2a86
Yaniv Elayev <ivielayev@gmail.com>
2024-05-13 01:26:03 +0300

    Rename db to file_reader

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.constants import SMALL_REQUEST_SIZE, LARGE_REQUEST_SIZE, FILE_PATH
-from src.db import DB
+from src.file_reader import FileReader
 
 
 def main():
-    db = DB(FILE_PATH)
+    db = FileReader(FILE_PATH)
     db.get(100, SMALL_REQUEST_SIZE)  # reads the chunk from the file and store it in the cache.
     # From the same offset, a 64 KB chunk will be stored for 2 seconds.
     db.get(100, SMALL_REQUEST_SIZE)  # reads the chunk from the cache.
@@ -11,7 +11,6 @@ def main():
     db.get(90, SMALL_REQUEST_SIZE)  # In case the cache is full, the oldest 64 KB chunk will be removed if there has
     # been 2 seconds since its insertion time. Else, the least recently used 8 KB chunk will be removed from the cache
     # in order to make room for the new chunk.
-
 
 if __name__ == '__main__':
     main()
